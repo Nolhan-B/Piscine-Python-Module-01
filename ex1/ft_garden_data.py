c1d62851eca0a9406dc3afcb0e7b73afed235586
nbilyj <nbilyj@k2r3p9.42mulhouse.fr>
2026-01-26 09:59:59 +0100

    rectif ex01

--- ex1/ft_garden_data.py
+++ ex1/ft_garden_data.py
@@ -1,18 +1,18 @@
+class Plant:
+    """Représente une plante du jardin."""
+    def __init__(self, name, height, age) -> None:
+        """Initialise une plante avec un nom, une taille et un âge."""
+        self.name = name
+        self.height = height
+        self.age = age
+
+    def present(self) -> None:
+        """Affiche les informations de la plante."""
+        print(f"{self.name}: {self.height}cm, {self.age} years old")
+
+
 def ft_garden_data() -> None:
     """Gère et affiche les données des plantes du jardin."""
-
-    class Plant:
-        """Représente une plante du jardin."""
-
-        def __init__(self, name, height, age) -> None:
-            """Initialise une plante avec un nom, une taille et un âge."""
-            self.name = name
-            self.height = height
-            self.age = age
-
-        def present(self) -> None:
-            """Affiche les informations de la plante."""
-            print(f"{self.name}: {self.height}cm, {self.age} years old")
 
     print("=== Garden Plant Registry ===")
 
